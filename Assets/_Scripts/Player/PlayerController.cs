3a31ac85930e0a53d236f9c7266dffef87d7759f
TuanAnhKazuto <tuananhkazuto@gmail.com>
2025-05-21 16:19:38 +0700

    Complete character jump animation

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -104,15 +104,21 @@ public class PlayerController : MonoBehaviour
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("IsFalling", false);
+            animator.SetBool("IsGrounded", true);
         }
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            animator.SetBool("IsJumping", true);
+            animator.SetBool("IsGrounded", false);
         }
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+        animator.SetBool("IsFalling", !isGrounded && velocity.y < 0);
     }
 
     private void Dash()
