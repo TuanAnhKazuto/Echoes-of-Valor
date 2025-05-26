64d78fb49d923e31d6ff37355e050dae20089877
HuynhCuongg <huynhcuongvan2004@gmail.com>
2025-05-27 01:15:06 +0700

    Inventory sử dụng Item trong Inventory

---
+++ Assets/_Scripts/invetory/InventoryManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance { get; private set; }
+
+    public List<Item> items = new List<Item>();
+
+    public Transform itemContentPane;
+    public GameObject itemPrefab;
+
+    private void Awake()
+    {
+        if (Instance != null || Instance != this)
+        {
+            Destroy(Instance);
+        }
+
+        Instance = this;
+        //DontDestroyOnLoad(gameObject);
+    }
+
+
+    public void Add(Item item)
+    {
+        items.Add(item);
+        DisplayInventory();
+    }
+
+    public void Remove(Item item)
+    {
+        items.Remove(item);
+    }
+
+    public void DisplayInventory()
+    {
+        foreach (Transform item in itemContentPane)
+        {
+            Destroy(item.gameObject);
+        }
+
+        foreach (Item item in items)
+        {
+            GameObject obj = Instantiate(itemPrefab, itemContentPane);
+            var itemName = obj.transform.Find("ItemName").GetComponent<TextMeshProUGUI>();
+            var itemImager = obj.transform.Find("ItemImage").GetComponent<Image>();
+
+            itemName.text = item.itemName;
+            itemImager.sprite = item.image;
+
+
+            //obj.GetComponent<ItemUIController>().SetItem(item);
+            
+            //Debug.Log("add item done");
+
+        }
+    }
+
+
+}
---
+++ Assets/_Scripts/invetory/ItemPickUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour
+{
+    public Item item;
+    
+    void PickUp()
+    {
+        Destroy(this.gameObject);
+        InventoryManager.Instance.Add(item);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PickUp();
+        }
+    }
+}
