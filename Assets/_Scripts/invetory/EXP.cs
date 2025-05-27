a73fd3c155ee020a3f73cbbb5f38e307a2fdc51d
HuynhCuongg <huynhcuongvan2004@gmail.com>
2025-05-27 12:38:33 +0700

    đang còn lỗi EXP

---
+++ Assets/_Scripts/invetory/EXP.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EXP : MonoBehaviour
+{
+    public int exp;
+
+    public TextMeshProUGUI expText;
+
+    public void IncreaseExp(int value)
+    {
+        exp += value;
+        expText.text = "Exp: " + exp.ToString();
+    }
+
+}
