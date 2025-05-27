cc6daa4914b88c14e5b304a29d374a08e22dd73d
tathetruong123 <truongttps40870@gmail.com>
2025-05-27 16:12:52 +0700

    Bom item, attack

---
+++ Assets/_Scripts/invetory/Item/BombThrower.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class BombThrower : MonoBehaviour
+{
+    public GameObject bombPrefab;           // Prefab quả bom
+    public Transform throwPoint;            // Vị trí ném (nên là vị trí trước mặt camera/player)
+    public float throwForce = 10f;          // Lực ném
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ThrowBomb();
+        }
+    }
+
+    void ThrowBomb()
+    {
+        GameObject bomb = Instantiate(bombPrefab, throwPoint.position, throwPoint.rotation);
+        Rigidbody rb = bomb.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.linearVelocity = throwPoint.forward * throwForce;
+        }
+    }
+}
