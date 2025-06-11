f986f477799578b082f4420e47ea9871ebca5085
HuynhCuongg <huynhcuongvan2004@gmail.com>
2025-06-12 02:42:43 +0700

    Xog Player Quest and Prefabs

---
+++ Assets/_Scripts/NPC/NpcChatSetup.cs
@@ -0,0 +1,10 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NpcChatSetup : MonoBehaviour
+{
+    public GameObject ChatPanel;
+    public TextMeshProUGUI ChatText;
+    public Button YesBtn;
+}
