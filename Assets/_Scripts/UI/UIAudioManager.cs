6b8634b3f71a89a23bf95e56110fb14bc3bdcb9d
HAUMEOOOO <haumdps40975@gmail.com>
2025-07-09 00:02:19 +0700

    update am thanh UI

---
+++ Assets/_Scripts/UI/UIAudioManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class UIAudioManager : MonoBehaviour
+{
+    public static UIAudioManager Instance;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // giữ lại giữa các scene nếu cần
+            audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.playOnAwake = false;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        if (clip != null)
+            audioSource.PlayOneShot(clip);
+    }
+}
