509f96cf88d0d101bd047a406485801af6f21bde
TuanAnhKazuto <tuananhkazuto@gmail.com>
2025-08-04 07:04:17 +0700

    UI change ingredient update weapon.

---
+++ Assets/_Scripts/Manager/SwithPanelController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SwithPanelController : MonoBehaviour
+{
+    public GameObject[] panels;
+
+    private void Start()
+    {
+        panels[0].SetActive(true); 
+        panels[1].SetActive(false);
+    }
+
+    public void SwitchToPanel(int panelIndex)
+    {
+        foreach (GameObject panel in panels)
+        {
+            panel.SetActive(false); // Tắt tất cả các panel
+        }
+        panels[panelIndex].SetActive(true); // Bật panel được chọn
+    }
+}
