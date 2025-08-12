e2aeeb110cbd4d96260fda39d36b477d802293d9
HuynhCuongg <huynhcuongvan2004@gmail.com>
2025-08-12 14:59:25 +0700

    hiện hình shop mua vật phẩm

--- Assets/_Scripts/UI/Shop/ShopItem/ShopItemUI.cs
+++ Assets/_Scripts/UI/Shop/ShopItem/ShopItemUI.cs
@@ -9,9 +9,14 @@ public class ShopItemUI : MonoBehaviour
     public Image itemIcon;
     public TextMeshProUGUI itemNameText;
     public TextMeshProUGUI priceText;
+    public ShopUIController shopUIController;
 
     private ShopItemProduct product;
 
+    private void Start()
+    {
+        shopUIController = FindAnyObjectByType<ShopUIController>();
+    }
     public void SetupUI(ShopItemProduct _product)
     {
         product = _product;
@@ -23,6 +28,7 @@ public class ShopItemUI : MonoBehaviour
     public void OnSelect()
     {
         // Gửi sản phẩm sang panel mua chi tiết
+        shopUIController.itemImage.sprite = itemIcon.sprite;
         Debug.Log("Chọn sản phẩm: " + product.itemData.itemName);
     }
 }
