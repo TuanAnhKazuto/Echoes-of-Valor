2f1241666d26d22f15e6bb29463cb5671cccfd31
HAUMEOOOO <haumdps40975@gmail.com>
2025-08-26 01:17:07 +0700

    Cổng dịch chuyển

---
+++ Assets/_Scripts/Tele/PortalDistance.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [Header("Cổng dịch chuyển")]
+    public Transform targetPortal;
+    public float offsetForward = 2f;  // dịch ra trước cổng
+    public float offsetUp = 1f;       // nhấc player lên
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // chỉ dịch chuyển Player
+        if (other.CompareTag("Player"))
+        {
+            Transform player = other.transform;
+
+            Vector3 targetPos = targetPortal.position
+                                + targetPortal.forward * offsetForward
+                                + Vector3.up * offsetUp;
+
+            player.position = targetPos;
+            player.rotation = targetPortal.rotation;
+
+            Debug.Log("Teleport tới: " + targetPortal.name);
+        }
+    }
+}
